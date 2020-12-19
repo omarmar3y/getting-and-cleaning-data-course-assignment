77ed784fb0117a42c0f7efedb1d79c4680cbef6c
omarmar3y <44435251+omarmar3y@users.noreply.github.com>
2020-12-19 19:05:58 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+---
+title: "README"
+output: html_document
+---
+The dataset includes the following files:
+=========================================
+- 'README.md'
+
+- 'run_analysis.R' : R script that download and unzip the data and do all the 
+                     required steps at the data outputting tidy data file 'means_set.txt'
+                     
+- 'means_set.txt' : text file contains the means of the standardevation and means
+                    of different measurements per subject and activity in tidy format
+                    
+- 'codebook.md' : a markdown file contains the indication about every variable
+                  and calculated summaries
---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+library(dplyr)
+## setwd() to the directory where you want to download the data
+
+
+
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip','./uci_dataset.zip')
+unzip('uci_dataset.zip')
+
+xtrain <- read.table('UCI HAR Dataset/train/X_train.txt')
+ytrain <- read.table('UCI HAR Dataset/train/y_train.txt')
+subtrain <- read.table('UCI HAR Dataset/train/subject_train.txt')
+
+xtest <- read.table('UCI HAR Dataset/test/X_test.txt')
+ytest <- read.table('UCI HAR Dataset/test/y_test.txt')
+subtest <- read.table('UCI HAR Dataset/test/subject_test.txt')
+
+train_set <- cbind(xtrain,ytrain,subtrain)
+test_set <- cbind(xtest,ytest,subtest)
+full_set <- rbind(train_set,test_set)
+
+features <- read.table('UCI HAR Dataset/features.txt')
+mean_std_indx <- grep('mean|std',features[[2]])
+mean_std_indx_act_sub <- c(mean_std_indx,562,563)
+
+mean_std_set <- full_set[,mean_std_indx_act_sub]
+names(mean_std_set) <- c(features[mean_std_indx,2],'activity','subject')
+
+acts <- read.table('UCI HAR Dataset/activity_labels.txt')
+mean_std_set$activity <- factor(mean_std_set$activity,levels = acts$V1,labels = acts$V2)
+
+means_set <- mean_std_set %>% group_by(activity,subject) %>% summarise_all(.funs =mean)
